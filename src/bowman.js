c17e5d1de9671a65a8577c76fe6c719fe51c7fff
Squid90 <squid90@rambler.ru>
2023-04-28 13:02:05 +0300

    start

---
+++ src/bowman.js
@@ -0,0 +1,9 @@
+import Character from './game';
+
+export default class Bowman extends Character {
+  constructor(name, type = 'Bowman') {
+    super(name, type);
+    this.attack = 25;
+    this.defence = 25;
+  }
+}
